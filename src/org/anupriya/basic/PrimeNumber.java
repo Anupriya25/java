2f99a0dd5a8fb3c3d7b98eb448416c852c47a8db
Anupriya <bhardwaj.anupriya7@gmail.com>
2017-09-23 14:29:50 +0530

    Prime Number

--- src/org/anupriya/basic/PrimeNumber.java
+++ src/org/anupriya/basic/PrimeNumber.java
@@ -4,18 +4,25 @@ public class PrimeNumber {
     public static void main(String args[]){
         System.out.println("Enter the number");
        // int number = Integer.parseInt(System.console().readLine());
-        System.out.println(isPrime(6));
+        System.out.println(isPrime(1));
     }
  public static boolean isPrime(int number){
         int sqrt= (int) Math.sqrt(number);
         boolean result=true;
-        for(int i=2;i<=sqrt;i++) {
-            if (number % i == 0) {
-                result = false;
-                break;
+           if(number==1){
+               return false;
+           }
+            for (int i = 2; i <= sqrt; i++) {
+                if (number % i == 0) {
+                    result = false;
+                    break;
+                }
             }
-        }
+
+
+
         return result;
+
         }
 
     }
